52fcae6b297c69ce9f80e0f721678acd964f963d
massomax <massocode@gmail.com>
2025-03-04 14:51:45 +0300

    Настройка базовой структуры проекта

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Подключение к MongoDB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+// Тестовый роут
+app.get('/', (req, res) => {
+  res.send('Сервер работает!');
+});
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
